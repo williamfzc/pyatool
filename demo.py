677e5ba7a76f6373db8fc86be89ded53ca296d6c
williamfzc <178894043@qq.com>
2018-10-26 23:39:46 +0800

    update demo

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 from pyat import PYAToolkit
 
 
-d = PYAToolkit('9c12aa96')
+d = PYAToolkit('123456F')
 d.bind(func_name='test_a', command='shell pm list package')
 result = d.test_a()
 print(result)
---
+++ manager_demo.py
@@ -0,0 +1,23 @@
+from pyat import PYAToolkit
+from whenconnect import when_connect, start_detect
+import time
+
+
+# register
+PYAToolkit.bind(func_name='test_a', command='shell pm list package')
+
+
+# when connect
+def add_device(device_id):
+    d = PYAToolkit(device_id)
+    result = d.test_a()
+    print(result)
+
+
+# detector
+when_connect(device='all', do=add_device)
+start_detect()
+
+# keep running
+while True:
+    time.sleep(1)
