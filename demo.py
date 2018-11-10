4765ac097baab24026212617fb2cf2e89e75ac20
williamfzc <178894043@qq.com>
2018-11-11 02:09:32 +0800

    update demo

--- demo.py
+++ demo.py
@@ -1,61 +1,76 @@
 from pyatool import PYAToolkit
 
 
-# real function should contains an argument named 'toolkit'
+# 个性化的函数需要toolkit形参，即使不需要使用
 def test_b(toolkit):
     return 'i am test_b, running on {}'.format(toolkit.device_id)
 
 
-# bind adb command
+# 封装adb命令成为方法
 PYAToolkit.bind_cmd(func_name='test_a', command='shell pm list package | grep google')
-# or a real function
+# 或者绑定个性化的函数
 PYAToolkit.bind_func(real_func=test_b)
 
-# change default configure
+# 修改配置
 PYAToolkit.change_conf('TAG_EXEC_CMD', 'EXEC_COMMAND')
 
-# need log? set it True or False
+# 是否需要log
 PYAToolkit.switch_logger(True)
 
-# init
+# 初始化
 d = PYAToolkit('123456F')
 
-# remote connect ( not very stable
+# 它也支持远程控制（还不够稳定，暂不推荐
 # d = PYAToolkit('123456F', mode='remote')
 
-# and call it
+# 已经绑定的方法直接调用即可
 result = d.test_a()
 # output (may be different)
 # package:com.google.android.webview
 
-# and call it too
+# 个性化函数也一样
 result = d.test_b()
 # i am test_b, running on 123456F
 
-# show all functions
+# 可以通过`standard_func`查询已有的标准函数（会有代码自动补全，比较方便），但不要通过它调用
+# d.standard_func.get_current_activity()
+# 应该直接这么调用
+d.get_current_activity()
+
+# 获取所有已经注册的函数
 all_functions = d.current_function()
 print(all_functions)
 
-# use build-in function
+# 下面列举所有标准函数的使用方法
 # test-only
 d.hello_world()
-# show all packages installed in your phone
-d.show_package()
-# top activity name
-d.get_current_activity()
-# install apk from url or path
+
+# 展示所有已安装的包
+installed_package = d.show_package()
+
+# 栈顶活动名
+current_activity_name = d.get_current_activity()
+
+# 安装指定apk（支持url与path）
 d.install_from(url=r'https://github.com/williamfzc/simhand2/releases/download/v0.1.2/app-debug.apk')
-# check if is installed
+d.install_from(path=r'/Users/admin/some_path/some_apk.apk')
+
+# 检测包是否已安装
 target_package_name = 'com.github.williamfzc.simhand2'
 is_installed = d.is_installed(package_name=target_package_name)
+
+# 清理缓存
 d.clean_cache(target_package_name)
 if is_installed:
     d.uninstall(target_package_name)
 
+# 切换wifi状态
 d.switch_wifi(False)
+# 切换飞行模式
 d.switch_airplane(True)
 d.switch_airplane(False)
 d.switch_wifi(True)
 
+# 获取手机ip
 local_address = d.get_ip_address()
 print(local_address)
