a9da038dc1c7c70af852a2dc21a93cdc751835bb
williamfzc <178894043@qq.com>
2018-11-21 14:25:29 +0800

    update v0.3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyatool',
-    version='0.3.2',
+    version='0.3.3',
     description='python android toolkit',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
