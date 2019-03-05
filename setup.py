1ed08a542701bea1d25bba88e384f8d10dead905
williamfzc <178894043@qq.com>
2019-03-05 17:46:54 +0800

    release: 0.3.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyatool',
-    version='0.3.5',
+    version='0.3.6',
     description='python android toolkit',
     author='williamfzc',
     author_email='fengzc@vip.qq.com',
